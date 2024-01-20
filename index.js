aaa9e186f81d16e314b5eb2e53a418d488d7599c
YeongHyeon Jeong <dudgus7495@gmail.com>
2024-01-20 14:54:37 -0500

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5050;
+
 
 app.use(express.json());
 
